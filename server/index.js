fb9bc68bc918f1aa5f0599e164bc1ca44874c8c5
SadieAnn <127803877+SadieStaar@users.noreply.github.com>
2025-10-04 17:43:34 -0500

    Create registration function

--- server/index.js
+++ server/index.js
@@ -2,34 +2,74 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 const bcrypt = require("bcrypt");
+const crypto = require("crypto");
 
-//  ENVIRONMENT VARIABLES
+//  ENVIRONMENT VARIABLE PROCESSING
 const PEPPER = String(process.env.PEPPER);
 const SQL_HOST = String(process.env.MYSQL_HOST);
 const SQL_USER = String(process.env.MYSQL_USER);
 const SQL_PASSWORD = String(process.env.MYSQL_PASSWORD);
 
+//  SQL REGEX STRINGS
+const UserLoginSQL = "SELECT password, salt FROM users WHERE username = ?";
+const ExistingUserSQLCheck = "SELECT username FROM users WHERE username = ?";
+const UserRegistrationSQL = "INSERT INTO users (username, password, salt) VALUES (?, ?, ?)";
+
 //  OTHER CONSTANTS
 const UserPasswordREGEX = /["':;(){}|\/\\]/;
 
-//  SQL Connection
+//  SQL CONNECTION
 const UserConnection = mysql.createConnection({
     host: SQL_HOST,
     user: SQL_USER,
     password: SQL_PASSWORD,
     database: "users",
 });
-
+// SQL startup health check
 UserConnection.ping(err => {
   if (err) console.error("Ping failed:", err);
   else console.log("DB connection OK");
 });
 
-// SQL Regex Strings
-const UserLoginSQL = "SELECT password, salt FROM users WHERE username = ?";
-const UserRegistrationSQL = "INSERT INTO users (username, password, salt) VALUES (?, ?, ?)";
+// REGISTRATION
+function register(usernameInput, passwordInput) {
+    return new Promise((resolve, reject) => {
+        // Validate input
+        if (UserPasswordREGEX.test(usernameInput) || UserPasswordREGEX.test(passwordInput)) {
+            return reject(new Error("Invalid characters in username or password"));
+        }
+        console.log(`Registering new user \" ${usernameInput}\"...`);
 
-// Login Function    
+        // Check if username has been taken
+        UserConnection.query(ExistingUserSQLCheck, [usernameInput], (SQLerror, SQLresults) =>{
+            if (SQLerror) {
+                return reject(new Error("Database error: " + SQLerror.message));
+            }
+
+            if (SQLresults.length !== 0) {
+                return reject(new Error("User already exists"));
+            }
+        })
+        console.log("Successful query. Inserting new user...");
+
+        // Create unique 12-byte salt and hash user password
+        let newSalt = crypto.randomBytes(Math.ceil(12 / 2))
+        .toString("hex")
+        .slice(0, 12);
+
+        bcrypt.hash(newSalt + passwordInput + PEPPER, 12)
+        .then(hashedPassword => {
+            UserConnection.query(UserRegistrationSQL, [usernameInput, hashedPassword, newSalt], (SQLerror, SQLresults) => {
+                if (SQLerror) {
+                return reject(new Error("Database error: " + SQLerror.message));
+                }
+                return resolve(`User account ${usernameInput} successfully created.`);
+            })
+        });
+    })
+}
+
+// LOGIN  
 function login(usernameInput, passwordInput) {
     return new Promise((resolve, reject) => {
 
@@ -37,7 +77,6 @@ function login(usernameInput, passwordInput) {
         if (UserPasswordREGEX.test(usernameInput) || UserPasswordREGEX.test(passwordInput)) {
             return reject(new Error("Invalid characters in username or password"));
         }
-
         console.log(`Login attempt for user: "${usernameInput}"`);
 
         // Perform SQL query
@@ -67,7 +106,3 @@ function login(usernameInput, passwordInput) {
         });
     });
 }
-
-login("testuser", "password")
-    .then(user => console.log("User found:", user))
-    .catch(err => console.error("Login failed:", err.message));
