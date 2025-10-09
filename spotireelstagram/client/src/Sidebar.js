34de32789089bac3290c299158839c1a45ea62bb
zayhindss <145375567+zayhindss@users.noreply.github.com>
2025-10-09 18:28:53 -0500

    Update and rename sidebar.js to Sidebar.js
    resized things so it fit the new playerbar dimensions, also so it stretched across screen and is now global, instead of fixed to the homepage.

--- spotireelstagram/client/src/Sidebar.js
+++ spotireelstagram/client/src/Sidebar.js
@@ -1,25 +1,28 @@
 import { FaHome, FaSearch, FaBook, FaPlus, FaFilm } from "react-icons/fa"
 
 const Sidebar = () => {
-    const go = (path) => () => { window.location.pathname = path; };
+    const go = (path) => () => { 
+        window.history.pushState({}, '', path);
+        window.dispatchEvent(new PopStateEvent('popstate'));
+    };
     return (
         <div
-            style={{
-                position: "fixed",
-                top: 70,
-                left: 10,
-                width: "200px",
-                height: "76vh",
-                backgroundColor: "#050505",
-                color: "white",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                paddingTop: "20px",
-                borderRight: "1px solid #050505",
-                zIndex: 100
-            }}
-            >
+                style={{
+                    position: "fixed",
+                    top: 0,
+                    left: 0,
+                    width: "220px",
+                    height: "100vh",
+                    backgroundColor: "#000000ff",
+                    color: "white",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    paddingTop: "20px",
+                    borderRight: "1px solid #000000ff",
+                    zIndex: 100
+                }}
+                >
             <h4 style={{ color: "#8e2dd2ff", marginBottom: "40px"}}>SpotiReelsTagram</h4>
             <div
                 style={{
@@ -29,7 +32,7 @@ const Sidebar = () => {
                     width: "100%"
                 }}
                 >
-                    <button style={navButtonStyle}><FaHome />Home</button>
+                    <button style={navButtonStyle} onClick={go("/home")}><FaHome />Home</button>
                     <button style={navButtonStyle} onClick={go("/search")}><FaSearch/>Search</button>
                     <button style={navButtonStyle}><FaBook/>Library</button>
                     <button style={navButtonStyle}><FaPlus/>Playlist</button>
