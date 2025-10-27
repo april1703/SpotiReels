fa9f7602011549d0c6da606aca4d5edb183dc055
SadieAnn <127803877+SadieStaar@users.noreply.github.com>
2025-10-27 14:23:43 -0500

    SQL Hotfix

--- sql/spottireels_database.sql
+++ sql/spottireels_database.sql
@@ -47,14 +47,20 @@ CREATE TABLE liked_songs (
 )
 
 -- FOR DEBUGGING PURPOSES ONLY, WILL BE REMOVED LATER
-INSERT INTO users VALUES
-("testuser1",
- "$2b$12$iE0iYgAs47Z/ROtJlzzOWeoFCE4nvHBFrudSTEicDbuuVCNmo4yBy",
- "basicsalt123", TRUE, TRUE),
-("testuser2",
- "$2b$12$iE0iYgAs47Z/ROtJlzzOWeoFCE4nvHBFrudSTEicDbuuVCNmo4yBy",
- "basicsalt123", TRUE, TRUE);
+-- INSERT INTO users VALUES
+-- ("testuser1",
+--  "$2b$12$iE0iYgAs47Z/ROtJlzzOWeoFCE4nvHBFrudSTEicDbuuVCNmo4yBy",
+--  "basicsalt123", 
+--  TRUE, 
+--  TRUE);
 
-INSERT INTO following (username, user_following) VALUES
-("testuser1", "testuser2"),
-("testuser2", "testuser1");
+-- INSERT INTO users VALUES
+-- ("testuser2",
+--  "$2b$12$iE0iYgAs47Z/ROtJlzzOWeoFCE4nvHBFrudSTEicDbuuVCNmo4yBy",
+--  "basicsalt123", 
+--  TRUE, 
+--  TRUE);
+
+-- INSERT INTO following (username, user_following) VALUES
+-- ("testuser1", "testuser2"),
+-- ("testuser2", "testuser1");
