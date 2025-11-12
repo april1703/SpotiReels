bf4858a3ce20f3ff557aac87906741ef956dafba
SadieAnn <127803877+SadieStaar@users.noreply.github.com>
2025-11-11 22:38:25 -0600

    fixed a lil bug rq

--- spotireelstagram/server/server.js
+++ spotireelstagram/server/server.js
@@ -75,7 +75,7 @@ const SQL_REQUESTS = {
     get_following: "SELECT p.* FROM posts p JOIN following f ON f.user_following = p.username WHERE f.username = ? ORDER BY p.time_stamp DESC;",
     add_comment: "INSERT INTO comments (post_ID, username, body, time_stamp) VALUES (?, ?, ?, ?);",
     get_comments: "SELECT username, body, time_stamp FROM comments WHERE post_ID = ? ORDER BY time_stamp ASC;",
-    get_random: "SELECT p.* FROM posts p WHERE p.username NOT IN (SELECT f.user_following FROM following f WHERE f.username = ?) ORDER BY p.time_stamp DESC;"
+    get_random: "SELECT p.* FROM posts p WHERE p.username WHERE p.username != ? AND p.username NOT IN (SELECT f.user_following FROM following f WHERE f.username = ?) ORDER BY p.time_stamp DESC;"
   },
   reactions: {
     add: "INSERT INTO reactions (post_ID, username) VALUES (?, ?)",
@@ -789,7 +789,7 @@ app.post("/getRandomPosts", (request, response) => {
     if(checkRegex([username])) {
         return response.status(403).send("Invalid characters in request body: Access denied.")
     }
-    Connection.query(SQL_REQUESTS.post.get_random, [username], (SQLerror, SQLresults) => {
+    Connection.query(SQL_REQUESTS.post.get_random, [username, username], (SQLerror, SQLresults) => {
         if(SQLerror) {
             return response.status(500).send(`Database error: ${SQLerror.message}`)
         } else if (SQLresults.length === 0) {
